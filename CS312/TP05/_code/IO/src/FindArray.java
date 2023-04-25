ec4489f1a36cce33fd9f1b6de5b92f87f1340682
lol <lol@lol>
2023-04-25 11:17:48 +0200

    j'ai faim

--- CS312/TP05/_code/IO/src/FindArray.java
+++ CS312/TP05/_code/IO/src/FindArray.java
@@ -7,17 +7,27 @@ public class FindArray {
 
     public int findElement(int[] array, int element) throws NoSuchElementException {
         try {
-			for (int i = 0; i < array.length; i++) {
+            for (int i = 0; i < array.length; i++) {
                 if (i == element) {
                     System.out.println("Found");
                     return i;
                 }
             }
             System.out.println("Not found");
-		} 
-        catch(Exception e) {
+        } catch (Exception e) {
             throw new NoSuchElementException();
         }
+        return -1;
+    }
+
+    public int findPattern(String[] array) throws NoSuchElementException {
+        for (String s : array) {
+            try {
+                return Integer.parseInt(s);
+            } catch (Exception e) {}
+        }
+        System.out.println("Not found");
+        throw new NoSuchElementException();
         return -1;
     }
 }
