669c3b331024e8b7f0fc654a51124f7ebf5b3ebf
lol <lol@lol>
2023-04-25 10:47:00 +0200

    owo

--- CS312/TP05/_code/IO/src/FindArray.java
+++ CS312/TP05/_code/IO/src/FindArray.java
@@ -1,17 +1,23 @@
 package IO;
 
 import java.io.*;
+import java.util.NoSuchElementException;
 
 public class FindArray {
 
     public void findElement(int[] array, int element) {
         // TODO : implement this method
-        for (int i : array) {
-            if (i == element) {
-                System.out.println("Found");
-                return;
+        try {
+			for (int i : array) {
+                if (i == element) {
+                    System.out.println("Found");
+                    return;
+                }
             }
+            System.out.println("Not found");
+		} 
+        catch(Exception e) {
+            throw new NoSuchElementException();
         }
-        System.out.println("Not found");
     }
 }
