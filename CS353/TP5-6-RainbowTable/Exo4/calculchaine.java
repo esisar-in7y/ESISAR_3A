59321e31bc35a74b33c952a5ce417b42a46ac553
flolep2607 <24566964+flolep2607@users.noreply.github.com>
2023-05-07 19:30:32 +0200

    up

--- CS353/TP5-6-RainbowTable/Exo4/calculchaine.java
+++ CS353/TP5-6-RainbowTable/Exo4/calculchaine.java
@@ -2,11 +2,9 @@
 // package calculchaine;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class calculchaine {
 
@@ -39,126 +37,36 @@ public class calculchaine {
         }
         return res % 100_000_000;
     }
-    // private static String crackPassword(String targetHash, Noeud[] table,
-    // MessageDigest digest) throws NoSuchAlgorithmException {
-    // for (int i = 999; i >= 0; i--) {
-    // byte[] targetBytes = String2Bytes(targetHash);
-    // int targetInt = reduction(targetBytes, i);
-    // for (int j = i; j < 1000; j++) {
-    // targetBytes = String2Bytes(hash(String.format("%08d", targetInt), digest));
-    // targetInt = reduction(targetBytes, j);
-    // }
-    // Noeud node = get(table, targetInt);
 
-    // if (node != null) {
-    // int candidateInt = node.px;
-    // String candidateHash;
-
-    // for (int j = 0; j <= i; j++) {
-    // String candidateStr = String.format("%08d", candidateInt);
-    // if(
-    // candidateStr.equals("17441945") ||
-    // candidateStr.equals("45239142") ||
-    // candidateStr.equals("39175522")
-    // ){
-    // System.out.println("candidatePx:"+candidateStr);
-    // }
-    // candidateHash = hash(candidateStr, digest);
-
-    // if (candidateHash.equals(targetHash)) {
-    // return candidateStr;
-    // }
-
-    // byte[] candidateBytes = String2Bytes(candidateHash);
-    // candidateInt = reduction(candidateBytes, j);
-    // }
-    // }
-    // }
-
-    // return null;
-    // }
-
-    // private static String crackPassword(String targetHash, Noeud[] table,
-    // MessageDigest digest) throws NoSuchAlgorithmException {
-    // for (int i = 0; i < 1000; i++) {
-    // int p = reduction(String2Bytes(targetHash), i);
-    // Noeud noeud = get(table, p);
-
-    // if (noeud != null) {
-    // int p1 = noeud.px;
-    // int p2 = noeud.p999;
-    // for (int j = 0; j < 1000; j++) {
-    // String potentialPassword = String.format("%08d", p1);
-    // if(
-    // potentialPassword.equals("17441945") ||
-    // potentialPassword.equals("45239142") ||
-    // potentialPassword.equals("39175522")
-    // ){
-    // System.out.println("candidatePx:"+potentialPassword);
-    // }
-    // String potentialHash = hash(potentialPassword, digest);
-    // if (potentialHash.equals(targetHash)) {
-    // return potentialPassword;
-    // }
-    // p1 = reduction(String2Bytes(potentialHash), p2);
-    // p2++;
-    // }
-    // }
-
-    // targetHash = hash(String.format("%08d", reduction(String2Bytes(targetHash),
-    // i)), digest);
-    // }
-
-    // return "Password not found";
-    // }
-
-    private static String crackPassword(String targetHash, Noeud[] table, MessageDigest digest) {
-        System.out.println("Cracking " + targetHash);
-        byte[] targetBytes = String2Bytes(targetHash);
-        int targetP999 = 0;
-        Noeud targetNoeud;
-        int candidatePx;
+    private static String crackPassword(String targetHash, Noeud[] table, MessageDigest digest) throws NoSuchAlgorithmException {
+        byte[] targetBytes;
+        int targetInt;
+        int candidateInt;
         String candidateHash;
-        String candidatePwd;
         byte[] candidateBytes;
-        byte[] bytes;
-        String str;
-        try {
-            for (int i = 999; i >= 0; i--) {
-                targetP999 = reduction(targetBytes, i);
-                for (int j = i; j < 999; j++) {
-                //TODO
-                str = String.format("%08d", targetP999);
-                bytes = String2Bytes(hash(str,digest));
-                targetP999 = reduction(bytes,j);
-                }
-                targetNoeud = get(table, targetP999);
-                // System.out.println("targetNoeud:"+targetNoeud);
-                if (targetNoeud != null) {
-                    // System.out.println("Found " + targetNoeud.px + " " + targetNoeud.p999);
-                    candidatePx = targetNoeud.px;
-                    // System.out.println("targetHash:"+targetHash);
-                    for (int k = 1; k < 1000; k++) {
-                        candidatePwd = String.format("%08d", candidatePx);
-                        if (candidatePwd.equals("17441945")) {
-                            System.out.println("candidatePx:" + candidatePx + " targetPx:" + targetP999
-                                    + "candidatePwd:" + candidatePwd);
-                        }
-                        candidateHash = hash(candidatePwd, digest);
-                        // System.out.println("candidateHash:"+candidateHash);
-                        if (candidateHash.equals(targetHash)) {
-                            return candidatePwd;
-                        }
-                        candidateBytes = String2Bytes(candidateHash);
-                        // System.out.println("candidateBytes:"+candidateBytes);
-                        candidatePx = reduction(candidateBytes, k);
-                        // System.out.println("candidatePx:"+candidatePx);
+        for (int i = 999; i >= 0; i--) {
+            targetBytes = String2Bytes(targetHash);
+            targetInt = reduction(targetBytes, i);
+            for (int j = i+1; j < 1000; j++) {
+                targetBytes = String2Bytes(hash(String.format("%08d", targetInt), digest));
+                targetInt = reduction(targetBytes, j);
+            }
+            Noeud node = get(table, targetInt);
+
+            if (node != null) {
+                candidateInt = node.px;
+                for (int j = 0; j <= i; j++) {
+                    String candidateStr = String.format("%08d", candidateInt);
+                    candidateHash = hash(candidateStr, digest);
+                    if (candidateHash.equals(targetHash)) {
+                        return candidateStr;
                     }
+                    candidateBytes = String2Bytes(candidateHash);
+                    candidateInt = reduction(candidateBytes, j);
                 }
             }
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
         }
+
         return null;
     }
 
@@ -170,10 +78,7 @@ public class calculchaine {
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 String[] splited = data.split("\\s+");
-                // insert(table, Integer.parseInt(splited[1]),new
-                // Noeud(Integer.parseInt(splited[1]), Integer.parseInt(splited[0])));
-                insert(table, Integer.parseInt(splited[1]),
-                        new Noeud(Integer.parseInt(splited[0]), Integer.parseInt(splited[1])));
+                insert(table, Integer.parseInt(splited[1]),new Noeud(Integer.parseInt(splited[0]), Integer.parseInt(splited[1])));
             }
             myReader.close();
         } catch (FileNotFoundException e) {
@@ -203,7 +108,6 @@ public class calculchaine {
     }
 
     public static void main(String[] args) {
-        Random r = new Random(0);
         Noeud[] table = read();
         String[] to_find = {
                 "38251B4C8C210841C60CDE0B7E4C7A87",
