4d84cb0e52b27dc39a68dfc4e4ae564dfc9e3746
Flo <cenestpasmonmail@hidden.truc>
2023-05-13 18:26:08 +0200

    o

--- MA332/Exo.r
+++ MA332/Exo.r
@@ -1,70 +1,39 @@
-# Exercice 1 On  ́etudie l’arriv ́ee de v ́ehicules `a une barri`ere d’autoroute.
-# Le temps est mesur ́e en secondes rss. Les arriv ́ees `a la barri`ere de p ́eage
-# son enregistr ́ees, plusieurs jours d’affil ́ees, pendant des intervalles de temps
-# rt0,t0 Tsavec T 3600 rss(une heure), pour une heure de r ́ef ́erence t0
-# choisie. On note Nptqle nombre de v ́ehicules arriv ́es entre les instants t0 et
-# t0 t, pour 0 ¤t ¤T. Apr`es un examen attentifs de ces s ́eries temporelles,
-# il est d ́ecid ́e de mod ́eliser le processus d’arriv ́ees Nptqcomme un processus
-# de Poisson de param`etre λ. Le param`etre optimal obtenu avec la m ́ethode
-# du maximum de vraisemblance est λ 1
-# 3
-# s1.
-# (i) Faire une simulation des arriv ́ees sur rt0,t0 Ts, v ́erifier la distribution
-# exponentielle des intervalles inter-arriv ́ees et repr ́esenter le graphe de
-# l’application t ÞÑNptqobtenu pour cette r ́ealisation particuli`ere.
-# (ii) Calculer le nombre moyen d’arriv ́ees dans l’intervalle r0,Ts. Vous
-# r ́ealiserez une moyenne d’ensemble sur plusieurs simulations. Vous
-# v ́erifierez que le r ́esultat obtenu est en accord avec la th ́eorie (`a la
-# pr ́ecision et au taux de confiance pr`es). Pouvez-vous obtenir un r ́esultat
-# similaire avec moins de simulations, mais sur des intervalles rt0,t0 Ts
-# plus long? Pourquoi?
-# (iii) Vous allez maintenant v ́erifier la distribution uniforme des instants
-# d’arriv ́ee lorsque le nombre total d’arriv ́ees NpTqest connu. Pour ce
-# faire, vous consid ́ererez le cas T 60 et NpTq20. Vous proc ́ederez
-# en deux temps :
-# (a) v ́erifier que dans le cas g ́en ́eral, les instants d’arriv ́ees An sont
-# bien distribu ́es selon une loi Γpλ,nq
-# 2
-# (b) v ́erifier que, si vous vous limitez aux simulations pour lesquelles
-# Np60q20, les instants d’arriv ́ees An sont distribu ́es de mani`ere
-# uniforme sur r0,60s
-
-# Set the seed for reproducibility
+# Fixer la graine pour la reproductibilité
 set.seed(123)
 
-# Define the time interval and the parameter lambda
-T <- 3600 # seconds
-lambda <- 1/3 # arrivals per second
+# Définir l'intervalle de temps et le paramètre lambda
+T <- 3600 # secondes
+lambda <- 1/3 # arrivées par seconde
 
-# Simulate the inter-arrival times using an exponential distribution
-interarrival_times <- rexp(1000, rate = lambda)
+# Simuler les temps entre arrivées en utilisant une distribution exponentielle
+temps_entre_arrivees <- rexp(1000, rate = lambda)
 
-# Compute the arrival times from the inter-arrival times
-arrival_times <- cumsum(interarrival_times)
+# Calculer les temps d'arrivée à partir des temps entre arrivées
+temps_arrivees <- cumsum(temps_entre_arrivees)
 
-# Plot the histogram of the inter-arrival times
-hist(interarrival_times, breaks = 20, main = "Histogram of inter-arrival times", xlab = "Time (s)")
+# Tracer l'histogramme des temps entre arrivées
+hist(temps_entre_arrivees, breaks = 20, main = "Histogramme des temps entre arrivées", xlab = "Temps (s)")
 
-# Plot the arrival times as a function of time
-plot(arrival_times, 1:length(arrival_times), type = "s", main = "Arrivals over time", xlab = "Time (s)", ylab = "Number of arrivals")
+# Tracer les temps d'arrivée en fonction du temps
+plot(temps_arrivees, 1:length(temps_arrivees), type = "s", main = "Arrivées au fil du temps", xlab = "Temps (s)", ylab = "Nombre d'arrivées")
 
-# Compute the average number of arrivals in the time interval [0, T]
-num_sims <- 1000
-num_arrivals <- numeric(num_sims)
-for (i in 1:num_sims) {
-  interarrival_times <- rexp(1000, rate = lambda)
-  arrival_times <- cumsum(interarrival_times)
-  num_arrivals[i] <- sum(arrival_times <= T)
+# Calculer le nombre moyen d'arrivées dans l'intervalle de temps [0, T]
+num_simulations <- 1000
+num_arrivees <- numeric(num_simulations)
+for (i in 1:num_simulations) {
+  temps_entre_arrivees <- rexp(2000, rate = lambda)
+  temps_arrivees <- cumsum(temps_entre_arrivees)
+  num_arrivees[i] <- sum(temps_arrivees <= T)
 }
-mean_arrivals <- mean(num_arrivals)
-cat("Average number of arrivals in [0, T] =", mean_arrivals, "\n")
+moyenne_arrivees <- mean(num_arrivees)
+cat("Nombre moyen d'arrivées dans [0, T] =", moyenne_arrivees, "\n")
 
-# Compute the theoretical mean and variance of the number of arrivals in [0, T]
-mean_theoretical <- lambda * T
-var_theoretical <- lambda * T
-cat("Theoretical mean and variance of arrivals in [0, T] =", mean_theoretical, var_theoretical, "\n")
+# Calculer la moyenne et la variance théoriques du nombre d'arrivées dans [0, T]
+moyenne_theorique <- lambda * T
+variance_theorique <- lambda * T
+cat("Moyenne et variance théoriques des arrivées dans [0, T] =", moyenne_theorique, variance_theorique, "\n")
 
-# Compute the empirical mean and variance of the number of arrivals in [0, T]
-mean_empirical <- mean(num_arrivals)
-var_empirical <- var(num_arrivals)
-cat("Empirical mean and variance of arrivals in [0, T] =", mean_empirical, var_empirical, "\n")
+# Calculer la moyenne et la variance empiriques du nombre d'arrivées dans [0, T]
+moyenne_empirique <- mean(num_arrivees)
+variance_empirique <- var(num_arrivees)
+cat("Moyenne et variance empiriques des arrivées dans [0, T] =", moyenne_empirique, variance_empirique, "\n")
---
+++ MA332/Exo2_bon.r
@@ -0,0 +1,71 @@
+# sain vacc inf mort
+P = matrix(c(0.65, 0.20, 0.15, 0.00,  # État 0 (sain)
+             0.00, 1.00, 0.00, 0.00,  # État 1 (vacciné)
+             0.00, 1/3*0.9,  2/3*0.9,  0.10,  # État 2 (infecté)
+             0.00, 0.00, 0.00, 1.00), # État 3 (mort)
+           nrow = 4, ncol = 4, byrow = TRUE)
+
+# Paramètres
+n_sims = 10          # Nombre de simulations
+n_weeks = 5            # Nombre de semaines à simuler
+initial_population = c(1000, 0, 0, 0)  # Population initiale : 1000 sains, 0 vaccinés, 1 infecté, 0 morts
+
+# Fonction pour simuler la propagation de la maladie
+simulate_propagation <- function(P, initial_population, n_weeks) {
+  population <- initial_population
+  for (i in 1:n_weeks) {
+    population <- population %*% P
+    cat(population,"\n")
+  }
+  return (population)
+}
+
+# Effectuer les simulations
+simulations <- replicate(n_sims, simulate_propagation(P, initial_population, n_weeks))
+
+# Calculer les moyennes pour chaque état
+mean_sain <- mean(simulations[,1,])/10
+mean_vaccine <- mean(simulations[,2,])/10
+mean_infecte <- mean(simulations[,3,])/10
+mean_mort <- mean(simulations[,4,])/10
+
+# Afficher les résultats
+cat("Moyenne des individus sains après", n_weeks, "semaines :", mean_sain, "\n")
+cat("Moyenne des individus vaccinés après", n_weeks, "semaines :", mean_vaccine, "\n")
+cat("Moyenne des individus infectés après", n_weeks, "semaines :", mean_infecte, "\n")
+cat("Moyenne des individus morts après", n_weeks, "semaines :", mean_mort, "\n")
+
+
+# Q3
+
+simulate_experience <- function(P) {
+  state <- 0
+  time <- 0
+  while (state != 1 && state != 3) {
+    state <- sample(0:3, size=1, prob=P[state + 1,])
+    time <- time + 1
+  }
+  return(time)
+}
+
+n_experiences <- 10000
+times <- replicate(n_experiences, simulate_experience(P))
+mean_time <- mean(times)
+
+
+#Q4
+
+n_experiences <- 10000
+n_deaths <- 0
+for (i in 1:n_experiences) {
+  state <- 0
+  while (state != 1 && state != 3) {
+    state <- sample(0:3, size=1, prob=P[state + 1,])
+  }
+  if (state == 3) {
+    n_deaths <- n_deaths + 1
+  }
+}
+prop_deaths <- n_deaths / n_experiences
+cat("probabilité mort:",prop_deaths*100,"%\n")
+
