0a541e9ffb99b21f0e127992f1820655c47b459c
lol <lol@lol>
2023-05-14 11:41:29 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- MA332/Exo3.r
+++ MA332/Exo3.r
@@ -65,30 +65,128 @@ for(i in 1:length(T)){
 # Affichage des résultats
 cbind(T, mean_clients) # Nombre moyen de clients dans le système en fonction de T
 
-# Question 3
-rho <- seq(0.1, 0.9, by = 0.1)
-Q <- numeric(length(rho))
-for(i in 1:length(rho)){
-  n_sim <- 1000 # Nombre de simulations
-  n_clients <- numeric(n_sim)
-  for(j in 1:n_sim){
-    # Simulation
+
+# Parameters
+lambda <- seq(0.001, 2, by = 0.01)
+mu <- 3
+
+# Function to calculate Q and W
+calculate_Q_W <- function(lambda, mu) {
+  nrep <- 1000
+  sum_Q <- 0
+  sum_W <- 0
+  
+  for (i in 1:nrep) {
     t <- 0
     n <- 0
-    while(t < T){
-      alpha <- rho[i]*lambda*(1-n/1)
-      beta <- mu*n
-      u <- runif(1)
-      if(u < alpha/(alpha+beta)){
-        t <- t + rexp(1, rate = alpha+beta)
+    Q <- 0
+    W <- 0
+    arrivals <- rexp(1, lambda)
+    departures <- Inf
+    
+    while (t < 1000) {
+      if (arrivals < departures) {
         n <- n + 1
-      }else{
-        t <- t + rexp(1, rate = alpha+beta)
+        t <- arrivals
+        Q <- Q + (n - 1)
+        W <- W + (n - 1) / lambda
+        arrivals <- arrivals + rexp(1, lambda)
+      } else {
         n <- n - 1
+        t <- departures
+        if (n > 0) {
+          departures <- t + rexp(1, mu)
+        } else {
+          departures <- Inf
+        }
       }
     }
-    # Nombre de clients dans le système en régime stationnaire
-    n_clients[j] <- n
+    
+    sum_Q <- sum_Q + Q / t
+    sum_W <- sum_W + W / t
   }
-  # Calcul de Q en régime stationnaire
-  Q[i] <- mean(n_clients)/rho[i] -
+  
+  Q <- sum_Q / nrep
+  W <- sum_W / nrep
+  
+  return(list(Q = Q, W = W))
+}
+
+# Calculate Q and W for different values of lambda
+results <- data.frame(lambda = lambda, Q = NA, W = NA)
+
+for (i in 1:length(lambda)) {
+  temp <- calculate_Q_W(lambda[i], mu)
+  results$Q[i] <- temp$Q
+  results$W[i] <- temp$W
+}
+
+# Plot the results
+library(ggplot2)
+
+ggplot(results, aes(x = lambda)) +
+  geom_line(aes(y = Q, colour = "Q")) +
+  geom_line(aes(y = W, colour = "W")) +
+  scale_colour_manual(values = c("Q" = "red", "W" = "blue")) +
+  xlab("Arrival rate (lambda)") +
+  ylab("Number of customers (Q) / Time spent in system (W)") +
+  ggtitle("Queueing System Performance") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+       
+# Question 3b
+rho_values <- seq(0.1, 0.95, by = 0.05)
+T <- 1000
+T_values <- seq(100, T, by = 100)
+
+# Fonction pour simuler la file M/M/1
+mm1_simulation <- function(lambda, mu, T) {
+  # Initialisation
+  n <- 0
+  n_arrivals <- 0
+  n_departures <- 0
+  arrivals <- rexp(1, rate = lambda)
+  departures <- Inf
+  
+  # Simulation
+  while (arrivals <= T || departures <= T) {
+    if (arrivals < departures) {
+      n_arrivals <- n_arrivals + 1
+      n <- n + 1
+      arrivals <- arrivals + rexp(1, rate = lambda)
+    } else {
+      n_departures <- n_departures + 1
+      n <- n - 1
+      if (n >= 1) {
+        departures <- departures + rexp(1, rate = mu)
+      } else {
+        departures <- Inf
+      }
+    }
+  }
+  
+  # Calcul de la moyenne du nombre de clients dans le système
+  mean_clients <- (1 / T) * sum(cumsum(c(rep(1, n_arrivals), rep(-1, n_departures)))) 
+  
+  return(mean_clients)
+}
+
+# Fonction pour calculer le paramètre opérationnel
+op_param <- function(lambda, mu, T) {
+  Q_T <- rep(0, length(T))
+  for (i in 1:length(T)) {
+    Q_T[i] <- mm1_simulation(lambda, mu, T[i])
+  }
+  
+  return(mean(Q_T))
+}
+
+# Calcul et représentation graphique du paramètre opérationnel en fonction de rho
+op_rho <- rep(0, length(rho_values))
+for (i in 1:length(rho_values)) {
+  lambda <- rho_values[i] * 2
+  mu <- 3
+  op_rho[i] <- op_param(lambda, mu, T)
+}
+
+plot(rho_values, op_rho, type = "l", xlab = "rho", ylab = "Opérationnel")
