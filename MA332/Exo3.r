52ed9c80bb42e5da22acc1e1789afce2fe0d6097
flolep2607 <24566964+flolep2607@users.noreply.github.com>
2023-05-14 22:00:11 +0200

    images

--- MA332/Exo3.r
+++ MA332/Exo3.r
@@ -1,78 +1,182 @@
+set.seed(0)
 # Paramètres du modèle
-lambda <- 2 # Taux d'arrivée
-mu <- 3 # Taux de service
-
-# Durée de la simulation
-T <- 20
+temps_total = 20
+lambda = 2
+mu = 3
 
 # Initialisation
-t <- 0
-n <- 0 # Nombre de clients dans la file (buffer + serveur)
-X <- data.frame(t, n)
+liste_temps = array(dim = 0)
+nombre_clients = array(dim = 0)
+
+# Variable temporaire pour la file
+nombre_clients_actuel = 0
+temps = 0
 
 # Simulation
-while(t < T){
-  # Calcul des taux d'arrivée et de service
-  alpha <- lambda*(1-n/1)
-  beta <- mu*n
+while (temps < temps_total) {
+  liste_temps = append(liste_temps, temps)
+  nombre_clients = append(nombre_clients, nombre_clients_actuel)
   
-  # Événement suivant
-  u <- runif(1)
-  if(u < alpha/(alpha+beta)){
-    # Arrivée d'un client
-    t <- t + rexp(1, rate = alpha+beta)
-    n <- n + 1
-  }else{
-    # Départ d'un client
-    t <- t + rexp(1, rate = alpha+beta)
-    n <- n - 1
+  temps_arrivee = rexp(1, lambda)
+  temps_service = rexp(1, mu)
+  
+  if (nombre_clients_actuel > 0) {
+    if (temps_arrivee < temps_service) {
+      temps = temps + temps_arrivee
+      nombre_clients_actuel = nombre_clients_actuel + 1
+    } else {
+      if (temps_arrivee != temps_service){
+        nombre_clients_actuel = nombre_clients_actuel - 1
+      }
+      temps = temps + temps_service
+    }  
+  } else {
+    temps = temps + temps_arrivee
+    nombre_clients_actuel = nombre_clients_actuel + 1
   }
-  
-  # Enregistrement de l'état du système
-  X <- rbind(X, data.frame(t, n))
 }
 
 # Graphique
-plot(X$t, X$n, type = "s", xlab = "Temps", ylab = "Nombre de clients dans la file")
+plot(liste_temps,nombre_clients, type = "s", xlab = "Temps", ylab = "Nombre de clients dans la file")
+
+
+
 
 # Question 2
-T <- seq(10, 100, by = 10)
-mean_clients <- numeric(length(T))
-for(i in 1:length(T)){
-  n_sim <- 100 # Nombre de simulations
-  n_clients <- numeric(n_sim)
-  for(j in 1:n_sim){
-    # Simulation
-    t <- 0
-    n <- 0
-    while(t < T[i]){
-      alpha <- lambda*(1-n/1)
-      beta <- mu*n
-      u <- runif(1)
-      if(u < alpha/(alpha+beta)){
-        t <- t + rexp(1, rate = alpha+beta)
-        n <- n + 1
-      }else{
-        t <- t + rexp(1, rate = alpha+beta)
-        n <- n - 1
-      }
-    }
-    # Nombre de clients dans le système à la fin de la simulation
-    n_clients[j] <- n
+T <- seq(0, 500, by = 5)
+file_simple <- function(temps_total){
+  temps_arrivee <- array(dim = 1)
+  temps_service <- array(dim = 1)
+  
+  # Remplir le tableau des temps d'arrivée
+  temps_arrivee[1] <- rexp(1, lambda)
+  t <- temps_arrivee[1]
+  i <- 2
+  while(t < temps_total){
+    tmp <- rexp(1, lambda)
+    if (tmp + t < temps_total)
+      temps_arrivee <- append(temps_arrivee, tmp + t)
+    t <- tmp + t
+    i <- i + 1
   }
-  mean_clients[i] <- mean(n_clients)
+  
+  # Remplir le tableau des temps de service
+  temps_service[1] <- rexp(1, mu)
+  t <- temps_service[1]
+  i <- 2
+  while(t < temps_total){
+    tmp <- rexp(1, mu)
+    if (tmp + t < temps_total)
+      temps_service <- append(temps_service, tmp + t)
+    t <- tmp + t
+    i <- i + 1
+  }
+  
+  # Initialiser les vecteurs à tracer
+  N <- array(dim = 1)
+  temps <- array(dim = 1)
+  N[1] <- 0
+  temps[1] <- 0
+  
+  # Construire les vecteurs à tracer à partir des temps d'arrivée et de service
+  i <- 1
+  j <- 1
+  k <- 2
+  while (!is.na(temps_arrivee[i]) && !is.na(temps_service[j])){
+    
+    # Un client arrive
+    if (temps_arrivee[i] < temps_service[j]){
+      temps <- append(temps, temps_arrivee[i])
+      N <- append(N, N[k-1] + 1)
+      i <- i + 1
+    }
+    
+    # Un client est servi
+    else if (temps_arrivee[i] > temps_service[j]){
+      temps <- append(temps, temps_service[j])
+      if (N[k-1] > 0)
+        N <- append(N, N[k-1] - 1)
+      else
+        N <- append(N, N[k-1])
+      j <- j + 1
+    }
+    
+    # Service se termine en même temps que l'arrivée d'un client
+    else if (temps_arrivee[i] == temps_service[j]){
+      temps <- append(temps, temps_arrivee[i])
+      N <- append(N, N[k-1])
+      i <- i + 1
+      j <- j + 1
+    }
+    
+    k <- k + 1
+  }
+  
+  # Terminer de parcourir les temps de service et d'arrivée
+  while (!is.na(temps_arrivee[i])){
+    temps <- append(temps, temps_arrivee[i])
+    N <- append(N, N[k-1] + 1)
+    i <- i + 1
+    k <- k + 1
+  }
+  while (!is.na(temps_service[j])){
+    temps <- append(temps, temps_service[j])
+    if (N[k-1] > 0)
+      N <- append(N, N[k-1] - 1)
+    else
+      N <- append(N, N[k-1])
+    j <- j + 1
+    k <- k + 1
+  }
+  
+  # Dernier point à t = temps_total
+  N <- append(N, N[k-1])
+  temps <- append(temps, temps_total)
+  
+  interTime = array(dim=0)
+  for (i in 1:length(temps)) {
+    interTime = append(interTime, temps[i+1] - temps[i])
+  }
+  interTime[length(temps)] = 0
+  return (weighted.mean(N, interTime))
 }
+
+
+moyenne_mm1 <- function(temps_total, nb_tests){
+  q <- 0
+  for (i in 1:nb_tests){
+    q <- q + file_simple(temps_total)
+  }
+  return (q/nb_tests)
+}
+moy <- rep(0, length(T))
+for (i in 1:length(T)) {
+  if(i!=0){
+    moy[i] <- moyenne_mm1(T[i],100);
+  }
+}
+moy[0] <- 0;
+rho = lambda / mu
+theorique = (rho/(1-rho))
+plot(T, moy, xlab="T (min)", ylab="Nombre de client moyen", main = "File M/M/1", col="blue", type="l")
+lines(T, array(data=(theorique),dim=length(T)), col="red", type="l", lwd = 2, lty = 2)
+legend("bottomright", legend=c("Moyenne mesurée", "Moyenne théorique"),col=c("blue", "red"), lty = 1:2, lwd=2)
+
+
+
+
 # Affichage des résultats
 cbind(T, mean_clients) # Nombre moyen de clients dans le système en fonction de T
 
 
+# Question 3a
 # Parameters
 lambda <- seq(0.001, 2, by = 0.01)
 mu <- 3
 
 # Function to calculate Q and W
 calculate_Q_W <- function(lambda, mu) {
-  nrep <- 1000
+  nrep <- 10 #1000
   sum_Q <- 0
   sum_W <- 0
   
@@ -121,19 +225,17 @@ for (i in 1:length(lambda)) {
   results$W[i] <- temp$W
 }
 
-# Plot the results
-library(ggplot2)
+# Plot the results using base R plot function
+plot(results$lambda, results$Q, type = "l", col = "red", ylim = c(0, max(results$Q, results$W)), 
+     xlab = "Temps d'arrivée (lambda)", 
+     ylab = "Nombre de client (Q) / Temps passé dans la queue (W)", 
+     main = "Performance du system de queue")
+lines(results$lambda, results$W, col = "blue")
+legend("topright", legend = c("Q", "W"), col = c("red", "blue"), lty = 1, bty = "n")
 
-ggplot(results, aes(x = lambda)) +
-  geom_line(aes(y = Q, colour = "Q")) +
-  geom_line(aes(y = W, colour = "W")) +
-  scale_colour_manual(values = c("Q" = "red", "W" = "blue")) +
-  xlab("Arrival rate (lambda)") +
-  ylab("Number of customers (Q) / Time spent in system (W)") +
-  ggtitle("Queueing System Performance") +
-  theme(plot.title = element_text(hjust = 0.5))
 
-       
+
+
 # Question 3b
 rho_values <- seq(0.1, 0.95, by = 0.05)
 T <- 1000
