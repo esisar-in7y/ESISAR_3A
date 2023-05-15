92725a98f4db618c3e5461bc23618a55df345eed
lol <lol@lol>
2023-05-15 21:54:30 +0200

    argh

---
+++ MA332/Exo3bon.r
@@ -0,0 +1,229 @@
+set.seed(0)
+# Paramètres du modèle
+temps_total = 20
+lambda = 2
+mu = 3
+
+# Initialisation
+liste_temps = array(dim = 0)
+nombre_clients = array(dim = 0)
+
+# Variable temporaire pour la file
+nombre_clients_actuel = 0
+temps = 0
+
+# Simulation
+while (temps < temps_total) {
+  liste_temps = append(liste_temps, temps)
+  nombre_clients = append(nombre_clients, nombre_clients_actuel)
+  
+  temps_arrivee = rexp(1, lambda)
+  temps_service = rexp(1, mu)
+  
+  if (nombre_clients_actuel > 0) {
+    if (temps_arrivee < temps_service) {
+      temps = temps + temps_arrivee
+      nombre_clients_actuel = nombre_clients_actuel + 1
+    } else {
+      if (temps_arrivee != temps_service){
+        nombre_clients_actuel = nombre_clients_actuel - 1
+      }
+      temps = temps + temps_service
+    }  
+  } else {
+    temps = temps + temps_arrivee
+    nombre_clients_actuel = nombre_clients_actuel + 1
+  }
+}
+
+# Graphique
+plot(liste_temps,nombre_clients, type = "s", xlab = "Temps", ylab = "Nombre de clients dans la file")
+
+
+# Question 2
+T <- seq(0, 500, by = 5)
+file_simple <- function(temps_total){
+  temps_arrivee <- array(dim = 1)
+  temps_service <- array(dim = 1)
+  
+  # Remplir le tableau des temps d'arrivée
+  temps_arrivee[1] <- rexp(1, lambda)
+  t <- temps_arrivee[1]
+  i <- 2
+  while(t < temps_total){
+    tmp <- rexp(1, lambda)
+    if (tmp + t < temps_total)
+      temps_arrivee <- append(temps_arrivee, tmp + t)
+    t <- tmp + t
+    i <- i + 1
+  }
+  
+  # Remplir le tableau des temps de service
+  temps_service[1] <- rexp(1, mu)
+  t <- temps_service[1]
+  i <- 2
+  while(t < temps_total){
+    tmp <- rexp(1, mu)
+    if (tmp + t < temps_total)
+      temps_service <- append(temps_service, tmp + t)
+    t <- tmp + t
+    i <- i + 1
+  }
+  
+  # Initialiser les vecteurs à tracer
+  N <- array(dim = 1)
+  temps <- array(dim = 1)
+  N[1] <- 0
+  temps[1] <- 0
+  
+  # Construire les vecteurs à tracer à partir des temps d'arrivée et de service
+  i <- 1
+  j <- 1
+  k <- 2
+  while (!is.na(temps_arrivee[i]) && !is.na(temps_service[j])){
+    
+    # Un client arrive
+    if (temps_arrivee[i] < temps_service[j]){
+      temps <- append(temps, temps_arrivee[i])
+      N <- append(N, N[k-1] + 1)
+      i <- i + 1
+    }
+    
+    # Un client est servi
+    else if (temps_arrivee[i] > temps_service[j]){
+      temps <- append(temps, temps_service[j])
+      if (N[k-1] > 0)
+        N <- append(N, N[k-1] - 1)
+      else
+        N <- append(N, N[k-1])
+      j <- j + 1
+    }
+    
+    # Service se termine en même temps que l'arrivée d'un client
+    else if (temps_arrivee[i] == temps_service[j]){
+      temps <- append(temps, temps_arrivee[i])
+      N <- append(N, N[k-1])
+      i <- i + 1
+      j <- j + 1
+    }
+    
+    k <- k + 1
+  }
+  
+  # Terminer de parcourir les temps de service et d'arrivée
+  while (!is.na(temps_arrivee[i])){
+    temps <- append(temps, temps_arrivee[i])
+    N <- append(N, N[k-1] + 1)
+    i <- i + 1
+    k <- k + 1
+  }
+  while (!is.na(temps_service[j])){
+    temps <- append(temps, temps_service[j])
+    if (N[k-1] > 0)
+      N <- append(N, N[k-1] - 1)
+    else
+      N <- append(N, N[k-1])
+    j <- j + 1
+    k <- k + 1
+  }
+  
+  # Dernier point à t = temps_total
+  N <- append(N, N[k-1])
+  temps <- append(temps, temps_total)
+  
+  interTime = array(dim=0)
+  for (i in 1:length(temps)) {
+    interTime = append(interTime, temps[i+1] - temps[i])
+  }
+  interTime[length(temps)] = 0
+  return (weighted.mean(N, interTime))
+}
+
+
+moyenne_mm1 <- function(temps_total, nb_tests){
+  q <- 0
+  for (i in 1:nb_tests){
+    q <- q + file_simple(temps_total)
+  }
+  return (q/nb_tests)
+}
+moy <- rep(0, length(T))
+for (i in 1:length(T)) {
+  if(i!=0){
+    moy[i] <- moyenne_mm1(T[i],100);
+  }
+}
+moy[0] <- 0;
+rho = lambda / mu
+theorique = (rho/(1-rho))
+plot(T, moy, xlab="T (min)", ylab="Nombre de client moyen", main = "File M/M/1", col="blue", type="l")
+lines(T, array(data=(theorique),dim=length(T)), col="red", type="l", lwd = 2, lty = 2)
+legend("bottomright", legend=c("Moyenne mesurée", "Moyenne théorique"),col=c("blue", "red"), lty = 1:2, lwd=2)
+
+
+
+
+# Affichage des résultats
+# cbind(T, mean_clients) # Nombre moyen de clients dans le système en fonction de T
+
+moyenne_mm1 <- function(lambda, mu, TT, nb_tests){
+  q <- 0
+  for (i in 1:nb_tests){
+    q <- q + file_simple(TT)
+  }
+  return (q/nb_tests)
+}
+
+moyenne_T_croissant_mm1 <-function(lambda, mu, TT_max, step){
+  q <- array(dim = 1)
+  TT <- array(dim = 1)
+  q[1] <- 0
+  TT[1] <- 0
+  
+  i<-step
+  while (i<=TT_max){
+    q <- append(q, moyenne_mm1(lambda, mu, i, 100))
+    TT <- append(TT, i)
+    i<-i+step
+  }
+  plot(TT, q, xlab="T (min)", ylab="q(T)", type="l")
+}
+
+evolution_Q_mm1 <- function(TT,mu,step){
+  i<-1
+  Q <- array(dim = 1)
+  rho <- array(dim = 1)
+  
+  Q[1]<-0
+  rho[1]<-0
+  
+  while(i<=2*mu){
+    Q <- append(Q, moyenne_mm1(i, mu, TT, 100))
+    rho <- append(rho, i/mu)
+    i<-i+step
+  }
+  plot(rho, Q, xlab="rho", ylab="q(T)", type="l")
+}
+
+evolution_qT_mm1 <- function(TT, mu, step){
+  i<-0
+  qT <- array(dim = 1)
+  rho <- array(dim = 1)
+  
+  qT[1]<-0
+  rho[1]<-0
+  i<-step
+  while(i<=2*mu){
+    q<-0
+    for (j in 1:100){ #TT
+      moy = moyenne_mm1(i, mu, j, 10)
+      if (!is.nan(moy))
+        q <- q + moy
+    }
+    qT <- append(qT, (1/TT)*q)
+    
+    rho <- append(rho, i/mu)
+    i<-i+step
+  }
+  plot(rho, qT, xlab="rho", ylab="qT(T)", type="l")
+}
